235226fca074264874c28b7194a40cd4bb7701e4
2405443-maker <2405443@kiit.ac.in>
2025-12-19 12:23:24 +0530

    1st problem of Lab2

---
+++ src/EvenOddCount.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        System.out.println("Enter 10 numbers:");
+
+        for (int i = 1; i <= 10; i++) {
+            int num = sc.nextInt();
+
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even numbers count = " + evenCount);
+        System.out.println("Odd numbers count  = " + oddCount);
+
+        sc.close();
+    }
+}
