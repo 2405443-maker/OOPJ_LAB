e34de1c3b8a0f9a0ffc539da038171c0ac796225
2405443-maker <2405443@kiit.ac.in>
2025-12-19 12:30:29 +0530

    2nd problem of Lab2

---
+++ src/objectCount.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class MyClass {
+    // static variable to count objects
+    static int count = 0;
+
+    // constructor
+    MyClass() {
+        count++;
+    }
+}
+
+public class objectCount {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of objects to create: ");
+        int n = sc.nextInt();
+
+        // creating objects
+        for (int i = 0; i < n; i++) {
+            new MyClass();
+        }
+
+        // displaying result
+        System.out.println("Number of objects = " + MyClass.count);
+
+        sc.close();
+    }
+}
